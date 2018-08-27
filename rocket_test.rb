4bc07d2782b5482293f3c86c7dd6b6f8ab45addf
curtischatch <curtis.chat.hk@gmail.com>
2018-08-27 14:07:34 -0400

    wrote test

--- rocket_test.rb
+++ rocket_test.rb
@@ -2,5 +2,36 @@ require "minitest/autorun"
 require_relative "rocket"
 
 class RocketTest < Minitest::Test
-  # Write your tests here!
+  def setup
+    @rocket = Rocket.new
+  end
+
+  def test_name_updater
+    @rocket.name = 'Cat'
+    expected = 'Cat'
+    actual = @rocket.name
+    assert_equal(expected, actual)
+  end
+
+  def test_colour_updater
+    @rocket.colour = 'blue'
+    expected = 'blue'
+    actual = @rocket.colour
+    assert_equal(expected, actual)
+  end
+
+  def test_lift_off
+    @rocket.lift_off
+    expected = true
+    actual = @rocket.flying?
+    assert actual
+  end
+
+  def test_landing
+    @rocket.lift_off
+    @rocket.land
+    expected = false
+    actual = @rocket.flying?
+    assert_equal(expected, actual)
+  end
 end
